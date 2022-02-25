71d80e962c99b22447b27c70762c0b31a04758c2
SameJess <jessicasantanacampos@gmail.com>
2022-02-25 16:33:45 -0300

    first commit

---
+++ ChromeAuto.py
@@ -0,0 +1,28 @@
+#Chrome Auto Tabs
+#Customize and open all your daily chrome tabs with just one click
+
+from re import X
+from tkinter import Button
+import pyautogui
+
+#Resolution Settings for mouse customized commands
+
+pyautogui.size()
+(1366,768)
+pyautogui.position()
+(605,750)
+pyautogui.click(x=605,y=750)
+pyautogui.click(button='left')
+
+#Opens Selected Program 
+
+pyautogui.hotkey('ctrl','n')
+
+#Opens Selected Tabs
+
+pyautogui.click(x=301,y=87)
+pyautogui.click(button='left', interval=0.25)
+
+pyautogui.hotkey('ctrl','t')
+
+
